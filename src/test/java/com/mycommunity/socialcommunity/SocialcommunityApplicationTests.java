0256a1812d752d33fbfba1981122eb18daf06b0c
dnjswndus95 <dnjswndus95@naver.com>
2022-07-09 14:51:55 +0900

    Project Start

---
+++ src/test/java/com/mycommunity/socialcommunity/SocialcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycommunity.socialcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
