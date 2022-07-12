821c1235e8b971a9d3678a0d1cb07ee5a39fe2c8
dnjswndus95 <dnjswndus95@naver.com>
2022-07-12 22:03:55 +0900

    도메인, BaseEntity 생성

---
+++ src/main/java/com/mycommunity/socialcommunity/domain/BaseTimeEntity.java
@@ -0,0 +1,44 @@
+package com.mycommunity.socialcommunity.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@MappedSuperclass  // Entity 공통속성을 매핑하기 위한 어노테이션
+@Getter
+@EntityListeners(AuditingEntityListener.class)  // EntityListeners : JPA Entity에 이벤트가 발생하면 콜백을 처리하고 코드를 실행하는 어노테이션
+                                                // Auditing 목적으로 사용할거라면 인자에 AuditingEntityListener.class를 인자로 넘겨준다.
+public abstract class BaseTimeEntity {
+
+    @Column(name = "created_date", nullable = false)
+    @CreatedDate
+    private String createdDate;
+
+    @Column(name = "modified_date", nullable = false)
+    @LastModifiedDate
+    private String modifiedDate;
+
+    // @EntityListeners 가 지원하는 콜백 메서드
+    /**
+     * 1. PrePersist / PostPersist : 엔티티를 저장하기 전
+     * 2. PreRemove / PostRemove : 엔티티를 삭제하기 전 / 후
+     * 3. PreUpdate / PostUpdate : 엔티티를 업데이트 하기 전 / 후
+     * 4. PostLoad : 해당 엔티티를 새로 불러오거나 refresh 한 후
+     **/
+
+    @PrePersist
+    public void onPrePersist(){
+        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
+        this.modifiedDate = this.createdDate;
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern(("yyyy.MM.dd HH:mm")));
+    }
+}
---
+++ src/main/java/com/mycommunity/socialcommunity/domain/User.java
@@ -0,0 +1,33 @@
+package com.mycommunity.socialcommunity.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @AllArgsConstructor : 모든 멤버변수를 파라미터로 받는 생성자
+ * @NoArgsConstructor : 파라미터가 없는 기본 생성자
+ * @RequiredArgsConstructor : final 이나 @Notnull인 멤버변수만 파라미터로 받는 생성자
+ */
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, length = 18)
+    private String password;
+
+
+}
