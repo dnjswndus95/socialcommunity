4d66d59b64be8eed6e95cd68ded5dfae4ef2797f
dnjswndus95 <dnjswndus95@naver.com>
2022-07-25 22:07:11 +0900

    Build.gradle 수정 및 Service 기능 구현

--- src/main/java/com/mycommunity/socialcommunity/application/service/PostsService.java
+++ src/main/java/com/mycommunity/socialcommunity/application/service/PostsService.java
@@ -1,4 +1,16 @@
 package com.mycommunity.socialcommunity.application.service;
 
+import com.mycommunity.socialcommunity.controller.repository.PostsRepository;
+import com.mycommunity.socialcommunity.controller.repository.UserRepository;
+import com.mycommunity.socialcommunity.domain.Posts;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+    private final UserRepository userRepository;
 }
--- src/main/java/com/mycommunity/socialcommunity/application/service/UserService.java
+++ src/main/java/com/mycommunity/socialcommunity/application/service/UserService.java
@@ -1,14 +1,26 @@
 package com.mycommunity.socialcommunity.application.service;
 
+import com.mycommunity.socialcommunity.application.dto.UserDto;
 import com.mycommunity.socialcommunity.controller.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
-   // private final UserRepository userRepository;
+   private final UserRepository userRepository;
+
+   private final BCryptPasswordEncoder encoder;  //비밀번호 암호화 메서드를 가진 클래스
+
+
+   @Transactional
+   public void joinUser(UserDto.Request dto){
+      dto.setPassword(encoder.encode(dto.getPassword()));
+      userRepository.save(dto.toEntity());
+   }
 
 
 }
